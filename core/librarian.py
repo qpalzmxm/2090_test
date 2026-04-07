799a99772d3e3a49873b8cd8f178068ca0e4a6a0
qpalzmxm <53147031+qpalzmxm@users.noreply.github.com>
2026-04-07 14:57:17 +0800

    Update librarian.py

--- core/librarian.py
+++ core/librarian.py
@@ -1,1 +1,13 @@
+from core.person import Person
 
+class Librarian(Person):
+    """Another subclass demonstrating Inheritance & Polymorphism"""
+
+    def __init__(self, librarian_id: str, name: str):
+        super().__init__(librarian_id, name)
+
+    def get_role(self) -> str:
+        return "Librarian"
+
+    def display_info(self) -> str:
+        return super().display_info() + " (Admin Access)"
